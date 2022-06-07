bee79945a99a3c96ccce5d9b8c96e65c6cefde7e
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2022-06-07 14:03:41 -0400

    forgot to remove wts debug msgs

--- yolov3_ultra/gen_wts.py
+++ yolov3_ultra/gen_wts.py
@@ -11,15 +11,7 @@ output = sys.argv[2]
 
 model = torch.load(weight_file, map_location="cuda:0")  # load to FP32
 model = model['ema' if model.get('ema') else 'model'].float()
-
-# update anchor_grid info
-#anchor_grid = model.model[-1].anchors * model.model[-1].stride[...,None,None]
-#model.model[-1].anchor_grid = anchor_grid
-#delattr(model.model[-1], 'anchor_grid')  # model.model[-1] is detect layer
-#model.model[-1].register_buffer("anchor_grid",anchor_grid) #The parameters are saved in the OrderDict through the "register_buffer" method, and then saved to the weight.
-
 model.to("cuda:0").eval()
-#print(model)
 
 true_idx = 0
 _p = 0
@@ -29,10 +21,8 @@ anchor_pt = 347
 anchor_loc = [347, 391, 435]
 anchor_loc_2 = [435, 391, 347]
 dict_len = len(model.state_dict().keys()) - 2 # -2 for anchor 
-#padding = 0
 
 idx_key = {} # weight key and wanted index:
-
 
 for i, (k, v) in enumerate(model.state_dict().items()):
     vr = v.reshape(-1).cpu().numpy()
@@ -62,14 +52,6 @@ for i, (k, v) in enumerate(model.state_dict().items()):
 
     if (i+1)%6 == 0: 
         true_idx += 1
-        #print("====================================")
-    #print(i, true_idx, k, vr.shape)
-
-
-#for i,(j,k) in enumerate(idx_key.items()):
-#    print(i, k, j)
-
-#exit()
 
 with open(output, 'w') as f:
     f.write('{}\n'.format(len(model.state_dict().keys())))
@@ -89,8 +71,6 @@ with open(output, 'w') as f:
         _n = '.'.join(_k)
         _name = "module_list.%i.%s"%(idx_key[k], _n)
         print(i, idx_key[k], _name, v.shape, vr.shape)
-        print(v)
-        input()
 
         f.write('{} {}'.format(
             _name,
